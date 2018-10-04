99a7f100c209ebefbabf25f45cf710ba3f7f5545
ElizaHlglh <ewu4@stuy.edu>
2018-10-03 21:59:45 -0400

    BankAccount.java

---
+++ BankAccount.java
@@ -0,0 +1,45 @@
+public class BankAccount{
+
+  private double balance;
+  private int accountID;
+  private String password;
+
+  public BankAccount(double x, int y, String z) {
+    balance = x;
+    accountID = y;
+    password = z;
+  }
+
+  public String toString() {
+    return accountID + "\t" +balance;
+  }
+
+  public void setPassword(String newPass) {
+    password = newPass;
+  }
+
+  public boolean deposit(double amount){
+    if (amount > 0) {
+      balance = balance + amount;
+      System.out.println("Deposit Success!");
+      return true;
+    }
+    else {
+      System.out.println("Deposit failure");
+      return false;
+    }
+  }
+
+  public boolean withdraw(double amount){
+    if (amount <= balance) {
+      balance = balance - amount;
+      System.out.println("Withdrawal success!");
+      return true;
+    }
+    else {
+      System.out.println("Withdrawal failure");
+      return false;
+    }
+  }
+
+}
